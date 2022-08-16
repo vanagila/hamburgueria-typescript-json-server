84ffd31457f90ba82d869e96d85aacc7cc0c15be
vanagila rodrigues <vanagilakedna@gmail.com>
2022-08-16 20:56:09 -0300

    styles: adds chakra theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,5 @@
-export const App = () => {};
+import { Heading } from "@chakra-ui/react";
+
+export const App = () => {
+  return <Heading>Hello world</Heading>;
+};
---
+++ src/styles/theme.ts
@@ -0,0 +1,37 @@
+import { extendTheme, theme as ChakraTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    primary: "#27AE60",
+    secondary: "#EB5757",
+    negative: "#E60000",
+    warning: "#FFCD07",
+    sucess: "#168821",
+    information: "#155BCB",
+    gray: {
+      600: "#333333",
+      300: "#828282",
+      100: "#E0E0E0",
+      0: "#F5F5F5",
+    },
+    fonts: {
+      heading: "Inter",
+      body: "Inter",
+    },
+    fontSizes: {
+      xm: "0.75rem",
+      sm: "0.875rem",
+      md: "1rem",
+      lg: "1.125rem",
+      xl: "1.375rem",
+      "2xl": "1.625rem",
+    },
+    styles: {
+      global: {
+        body: {
+          bg: "white",
+        },
+      },
+    },
+  },
+});
