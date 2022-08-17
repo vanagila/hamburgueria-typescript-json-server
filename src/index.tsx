206ed51d4d31f629e324be369a8e49c03bf48556
vanagila rodrigues <vanagilakedna@gmail.com>
2022-08-16 21:08:56 -0300

    feat: initial provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import { theme } from "./styles/theme";
-import { ChakraProvider } from "@chakra-ui/react";
+import { AppProvider } from "./providers";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
+    <AppProvider>
       <App />
-    </ChakraProvider>
+    </AppProvider>
   </React.StrictMode>
 );
