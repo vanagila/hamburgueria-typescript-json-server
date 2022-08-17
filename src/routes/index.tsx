b01f117bee6f2c87097fa353c525e35592d16a10
vanagila rodrigues <vanagilakedna@gmail.com>
2022-08-16 22:30:57 -0300

    feat: adds login route

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Switch } from "react-router-dom";
+
+import { Login } from "../pages/Login";
+
+export const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Login} />
+    </Switch>
+  );
+};
