7798b5b283ef78c6bc8e4d5ad64411e79059d9ca
vanagila rodrigues <vanagilakedna@gmail.com>
2022-08-23 23:35:37 -0300

    feat: adds header to the dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,11 @@
 import { Header } from "../../components/Header";
 
+import { useProducts } from "../../providers/Products";
+
 export const Dashboard = () => {
+  const { products } = useProducts();
+
+  console.log(products);
+
   return <Header />;
 };
